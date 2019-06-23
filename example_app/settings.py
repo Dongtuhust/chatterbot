0bc126ba95edbbc5b60717923df352bb8baf70fe
Dongtuhust <dongtu.hust@gmail.com>
2019-06-23 14:55:48 +0700

    add Procfile

--- example_app/settings.py
+++ example_app/settings.py
@@ -1,6 +1,8 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -104,3 +106,4 @@ STATICFILES_DIRS = (
         'static',
     ),
 )
+django_heroku.settings(locals())
