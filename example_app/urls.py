39c42f54be4c6cba2f3edbff69a3ba392f7dce89
Dongtuhust <dongtu.hust@gmail.com>
2019-06-23 15:02:49 +0700

    update static file

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from example_app.views import ChatterBotAppView, ChatterBotApiView
 
 
@@ -8,3 +10,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^api/chatterbot/', ChatterBotApiView.as_view(), name='chatterbot'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
